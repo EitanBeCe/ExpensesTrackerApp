32569a255c894bc4b5c2c48e499d7a7d3b950f11
Eitan <eitanleviberger@gmail.com>
2022-07-25 15:27:58 +0300

    normal date format

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,19 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem({date, title, amount}) {
+function ExpenseItem({ date, title, amount }) {
 	// const {date, title, amount} = props;
+  
+	const month = date.toLocaleString('en-US', { month: 'long' });
+  const day = date.toLocaleString('en-US', { day: '2-digit' });
+  const year = date.getFullYear();
 
   return (
     <div className='expense-item'>
-      <div>{date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className='expense-item__description'>
         <h2>{title}</h2>
         <div className='expense-item__price'>${amount}</div>
